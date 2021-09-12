7aca0eda0d2c96958ca62690fc7fa0fa9f90432e
Manuel Ferreras <35044975+ManuelFerreras@users.noreply.github.com>
2021-09-12 01:39:52 -0300

    Loader Finished

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const loader = document.querySelector('.loader');
+const cargador = document.querySelector('.cargador');
+const contenidoPagina = document.querySelector('.contenido-pagina');
+const body = document.querySelector('body');
+
+
+function init() {
+    setTimeout(() => {
+        loader.style.opacity = 0;
+        loader.style.display = 'none';
+
+        cargador.remove()
+        body.style.display = 'block';
+        contenidoPagina.style.display = 'block';
+        setTimeout(() => {
+            contenidoPagina.style.opacity = 1;
+        }, 50);
+    }, 4000);
+}
+
+init();
