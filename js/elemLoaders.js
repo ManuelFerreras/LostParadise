c5b1e32eba3d13f97c4753c38e0b5c17997e4ad6
Manuel Ferreras <35044975+ManuelFerreras@users.noreply.github.com>
2021-09-19 18:03:10 -0300

    faders

--- js/elemLoaders.js
+++
@@ -1,6 +0,0 @@
-const primero = document.querySelector('.first');
-const segundo = document.querySelector('.second');
-
-
-ScrollReveal().reveal(primero);
-ScrollReveal().reveal(segundo, { delay: 300 });
